be34fd12403701df00fb4a16f7b53a827f109ddd
yuri.karmaz <106161829+yuri69k@users.noreply.github.com>
2023-03-27 16:57:36 +0300

    add icon tel

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact} from 'Redux/contactsSlice';
 import { getFilter,getItems } from 'Redux/selectors';
 import userIcon from '../../images/user.png';
+import telephoneIcon from '../../images/telephone.png';
+import { Notification } from 'components/Notification/Notification';
 const Contacts = () => {
   const contacts = useSelector(getItems );
   const filter = useSelector(getFilter);
@@ -18,13 +20,21 @@ const Contacts = () => {
  
   return (
     <>
+        {contacts.length === 0 ? (
+        <Notification message="No contacts yet" />
+      ) : filter !== '' && rendered.length === 0 ? (
+        <Notification message="No contacts found" />
+      ) : (
       <ul className={styles.contactsList}>
         {rendered.map(({ name, id, number }) => (
           <li className={styles.listItem} key={id} id={id}>
               <img src={userIcon} alt="user icon" className={styles.user_icon} />
             <span className={styles.contactName}>{name}: </span>
-            <span className={styles.phoneNumber}>{number}</span>
-
+            <span className={styles.phoneNumber}>{number}
+            <a href={`tel:${number}`}>
+            <img src={telephoneIcon} alt="telephone icon" className={styles.telephone_icon} />
+              </a>
+              </span>
             <button
               type="button"
               className={styles.buttons}
@@ -35,10 +45,9 @@ const Contacts = () => {
             </button>
           </li>
         ))}
-      </ul>
-      {contacts.length === 0 && (
-        <p style={{ textDecoration: 'underline' }}>no contacts available</p>
-      )}
+        </ul>
+              )}
+      
     </>
   );
 };
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,8 +1,8 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { setFilter } from 'Redux/filterSlice';
-import{getFilter } from 'Redux/selectors'
+import{getFilter,getItems } from 'Redux/selectors'
 import styles from './styles.module.css';
-
+import clearIcon from '../../images/backspace.png';
 
 // const Filter = ({ onFilterInput }) => {
 //   const [filter, setFilter] = useState('');
@@ -16,15 +16,25 @@ export const Filter = () => {
   return (
     <div className={styles.wrapper}>
       <p className={styles.text}>Find contacts by name</p>
-        <input
+      <input
+        type="text"
         name="filter"
         value={filter}
         onChange={onChange}
         placeholder="Enter name"
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-  
+        disabled={useSelector(getItems).length === 0}
       />
+      {useSelector(getFilter) && (
+        <button
+          className={styles.button}
+          type="button"
+          onClick={() => dispatch(setFilter(''))}
+        >
+          <img src={clearIcon} alt="clear icon" className={styles.icon} />
+        </button>
+      )}
     </div>
   );
 };
